9cbe31d5c94fb76533ca61c864433cce06e98af8
AlexandreCare <70692837+AlexandreCare@users.noreply.github.com>
2021-03-23 17:24:47 +0100

    Add files via upload

--- fonction.php
+++ fonction.php
@@ -1,9 +1,10 @@
 <?php
+        $MaBase = null;
     //connection a la base
     try{
 
         $MaBase = new PDO("mysql:host=mysql-woolfty.alwaysdata.net; dbname=woolfty_virus; charset=utf8", "woolfty_site", "Ale761mioNW2002");
-
+        
     }catch(Exeption$e){
         echo $e->getMessage();
     }
@@ -18,6 +19,41 @@
         }
     }
     
+    //formulaire pour la connexion de l'utilisateur
+    function form($MaBase){
+        ?>
+            <p>
+                <?php
+                    //sélection du nombre d'utilisateur
+                    $nb = $MaBase->query("SELECT COUNT(*) FROM Users");
+                    $gens = $nb->fetch();
+
+                    echo '<h1>Déjà '.$gens['COUNT(*)'].' membres !</h1>';
+                ?>
+            </p>
+            <u>
+                <h2 class="gris">Connexion :</h2>
+            </u>
+            <input class="input" type="text" placeholder="Entrez votre Pseudo" name="nom" maxlength="10" required> <!--champ texte pour entrer sont pseudo-->
+
+            <input class="input" type="password" placeholder="Entrez votre mot de passe" name="MDP" required>
+
+            <input class="input" type="submit" name='submit' value="connexion">  <!-- bouton d'inscription-->
+            <?php
+                if (isset($_POST["submit"])) {
+
+                    $Result = $MaBase->query("SELECT * FROM `Users` WHERE `pseudo`='".$_POST['nom']."' AND `MDP` = '".$_POST['MDP']."'");
+                    if($tab = $Result->fetch()){ //selection des utilisateurs pour la connection
+                        //si il existe et que le mot de passe correspond -> connection
+                        $_SESSION["Logged"] = true;
+                        $_SESSION["idUser"] = $tab['id'];
+
+                        echo '<meta http-equiv="refresh" content="0">';
+                    }
+                }
+            ?>
+        <?php
+    }
     //fonction de déconnection
     function deco(){
         //deconection
@@ -30,7 +66,6 @@
         if(isset($_POST["deco"])){
             $_SESSION["Logged"] = false;
             session_destroy();
-            header("Location: index.php");
             echo '<meta http-equiv="refresh" content="0">';
         }
     }
--- index.php
+++ index.php
@@ -18,37 +18,8 @@
         <div class="arti3">
             <div class="form"> <!--classe css "form-->
                 <form class="form1" action="" method="post">
-                    <p>
-                        <?php
-                            //sélection du nombre d'utilisateur
-                            $nb = $MaBase->query("SELECT COUNT(*) FROM Users");
-                            $gens = $nb->fetch();
-
-                            echo '<h1>Déjà '.$gens['COUNT(*)'].' membres !</h1>';
-                        ?>
-                    </p>
-                    <u>
-                        <h2 class="gris">Connexion :</h2>
-                    </u>
-                    <input class="input" type="text" placeholder="Entrez votre Pseudo" name="nom" maxlength="10" required> <!--champ texte pour entrer sont pseudo-->
-
-                    <input class="input" type="password" placeholder="Entrez votre mot de passe" name="MDP" required>
-
-                    <input class="input" type="submit" name='submit' value="connexion">  <!-- bouton d'inscription-->
                     <?php
-                        if (isset($_POST["submit"])) {
-
-                            $Result = $MaBase->query("SELECT * FROM `Users` WHERE `pseudo`='".$_POST['nom']."' AND `MDP` = '".$_POST['MDP']."'");
-                            if($Result->rowCount()>0){ //selection des utilisateurs pour la connection
-
-                                $tab = $Result->fetch();
-                                //si il existe et que le mot de passe correspond -> connection
-                                $_SESSION["Logged"] = true;
-                                $_SESSION["idUser"] = $tab['id'];
-
-                                echo '<meta http-equiv="refresh" content="0">';
-                            }
-                        }
+                        form($MaBase);
                     ?>
                 </form>
             </div>
@@ -56,14 +27,13 @@
     </div>
     <?php
     }else{
-        $gens = $MaBase->query("SELECT * FROM Users");
+        $gens = $MaBase->query("SELECT * FROM Users WHERE `id`='".$_SESSION["idUser"]."'");
         $sos=$gens->fetch();
 
         echo "<h1>Bienvenue ".$sos["pseudo"]."</h1>";
 
         deco();
     }
-    
     ?>
 </body>
 </html>
